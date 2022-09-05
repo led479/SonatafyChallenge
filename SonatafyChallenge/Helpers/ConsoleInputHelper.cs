688c556d123615a029170d6aa830a149588123a1
Rolf @ Fetchly <rolf@fetch.ly>
2022-09-05 15:14:56 -0300

    Creates the main program console application

---
+++ SonatafyChallenge/Helpers/ConsoleInputHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonatafyChallenge
+{
+    public static class ConsoleInputHelper
+    {
+        private static readonly List<string> exemptItemNames = new List<string>
+        {
+            "book", "chocolate", "pill"
+        };
+
+        public static bool TryCreateItemFromConsoleInput(string input, out List<Item> items)
+        {
+            items = new List<Item>();
+
+            if (string.IsNullOrEmpty(input) || !input.Contains(" at ") || !input.Contains(" "))
+                return false;
+
+            int quantity;
+            if (!int.TryParse(input.Split(" ")[0], out quantity))
+                return false;
+
+            var quantityString = input.Split(" ")[0];
+            var nameWithQuantity = input.Split(" at ")[0].Trim();
+            var name = nameWithQuantity.Replace(quantityString, "").Trim();
+
+            var taxEnum = exemptItemNames.Any(x => name.ToLower().Contains(x)) ? TaxEnum.EXEMPT : TaxEnum.NORMAL;
+
+            var isImport = name.StartsWith("Imported");
+            decimal value;
+            if (!decimal.TryParse(input.Split(" at ")[1], out value))
+                return false;
+
+            for (int i = 0; i < quantity; i++)
+            {
+                var item = new Item
+                {
+                    Name = name,
+                    IsImported = isImport,
+                    TaxEnum = taxEnum,
+                    Value = value
+                };
+
+                items.Add(item);
+            }
+            
+            return true;
+        }
+
+    }
+}
---
+++ SonatafyChallenge/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SonatafyChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var salesReceipt = new SalesReceipt
+                {
+                    Items = new List<Item>()
+                };
+
+                var userSelection = "";
+
+                while (userSelection != "2")
+                {
+                    Console.WriteLine("\nWhat would you like to do? (Input the following number to proceed):");
+
+                    Console.WriteLine(" 1 - Add item to shopping basket");
+                    Console.WriteLine(" 2 - Print Sales Receipt and finish\n");
+
+                    userSelection = Console.ReadLine();
+
+                    if (userSelection == "1")
+                    {
+                        Console.WriteLine("\nPlease write the item that you would like to include in the following pattern:\n" +
+                            "(Prepend Imported to the name if it is an imported item)\n\n" +
+                            "   {Quantity} {Name} at {Value}\n\n");
+
+                        var userItemInput = Console.ReadLine();
+
+                        List<Item> newItems;
+                        if (ConsoleInputHelper.TryCreateItemFromConsoleInput(userItemInput, out newItems))
+                        {
+                            salesReceipt.Items.AddRange(newItems);
+                            Console.WriteLine($"\nInputted item has been successfully added to the shopping basket");
+
+                        }
+                        else
+                            Console.WriteLine("\nThe item isn't written in the correct pattern, please make sure to follow the pattern.");
+                    }
+                }
+
+                Console.WriteLine("\n============ SALES RECEIPT ============");
+                Console.WriteLine(salesReceipt.GetStringRepresentation());
+
+            } catch (ApplicationException ex)
+            {
+                Console.WriteLine($"\n\nAn error has occurred! Error description: {ex.Message}.");
+            }
+        }
+    }
+}
